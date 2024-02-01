4eff0778f4ff544d291ab385e089a51f2ad0180d
Tuan Pham <tuan-pham@cybozu.vn>
2024-02-01 15:46:53 +0700

    fix: fix bug 4

--- src/js/config.ts
+++ src/js/config.ts
@@ -10,7 +10,7 @@ jQuery.noConflict();
 
 (($, PLUGIN_ID) => {
   "use strict";
-  // fix bug 3g
+  // fix bug 4
   const CONF = kintone.plugin.app.getConfig(PLUGIN_ID);
 
   const saveSettings = (config: object) => {
