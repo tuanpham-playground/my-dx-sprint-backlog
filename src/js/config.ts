d8ea04d9f3ff6604461f4658e640e29bb6e7086b
tuan-pham <41720778+tuanphamcybozu@users.noreply.github.com>
2024-02-02 08:22:15 +0700

    fix: config ts 14 (#40)
    * fix: fix bug 14
    
    * fix: fix bug 14

--- src/js/config.ts
+++ src/js/config.ts
@@ -10,7 +10,7 @@ jQuery.noConflict();
 
 (($, PLUGIN_ID) => {
   "use strict";
-  // fix bug 13
+  // fix bug 14
   const CONF = kintone.plugin.app.getConfig(PLUGIN_ID);
 
   const saveSettings = (config: object) => {
